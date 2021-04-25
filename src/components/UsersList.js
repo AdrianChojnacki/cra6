a2899595fcb1818d6180f12996d8b4a815c5d265
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-25 14:16:21 +0200

    styles

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./UsersList.css";
 
 const UsersList = (props) => {
   const users = props.users.map((user) => (
-    <li key={user.login.uuid}>
+    <li key={user.login.uuid} className="user">
       <img src={user.picture.large} alt={user.name.last} />
+      <h4>{`${user.name.title} ${user.name.first} ${user.name.last}`} </h4>
       <h4>
-        {`${user.name.title} ${user.name.first} ${user.name.last}`}{" "}
         <a href={`mailto:${user.email}`}>{user.email}</a>
       </h4>
     </li>
