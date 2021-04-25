56b52c9b46be97e60006d878d2bf5091e4ef6577
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-25 14:05:17 +0200

    initial

---
+++ src/components/ButtonFetchUsers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonFetchUsers = (props) => {
+  return <button onClick={props.click}>Pokaż 5 Użytkowników</button>;
+};
+
+export default ButtonFetchUsers;
---
+++ src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UsersList = (props) => {
+  const users = props.users.map((user) => (
+    <li key={user.login.uuid}>
+      <h4>
+        {`${user.name.title} ${user.name.first} ${user.name.last}`}{" "}
+        <a href={`mailto:${user.email}`}>{user.email}</a>
+      </h4>
+    </li>
+  ));
+
+  return <ul>{users}</ul>;
+};
+
+export default UsersList;
