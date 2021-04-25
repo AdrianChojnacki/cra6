d94970a9d9176bef55bd299d3bf8a486b8326905
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-25 14:10:30 +0200

    added pics

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,6 +3,7 @@ import React from "react";
 const UsersList = (props) => {
   const users = props.users.map((user) => (
     <li key={user.login.uuid}>
+      <img src={user.picture.large} alt={user.name.last} />
       <h4>
         {`${user.name.title} ${user.name.first} ${user.name.last}`}{" "}
         <a href={`mailto:${user.email}`}>{user.email}</a>
